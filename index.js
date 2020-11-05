7f3be4d73649f142aa21dd9ff0f93f8c18954644
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2020-11-05 02:31:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
@@ -7,3 +8,7 @@ append.push('Odie');
 prepend.unshift('Odie');
 removeLast.pop();
 removeFirst.shift();
+=======
+// Write your solution here!
+ const append => [...pets, 'Odie'];
+>>>>>>> daea0eec553842a817c894337efa8b44f465b93c
